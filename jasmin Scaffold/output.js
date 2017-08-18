c909dca9768b2634164b592be9c274253c0e0a43
dev.singh <dev.singh@thomsonreuters.com>
2017-08-18 13:04:48 -0400

    adding some input and output samples

---
+++ jasmin Scaffold/output.js
@@ -0,0 +1,40 @@
+describe('a unit of code', function() {
+
+	describe('when correctly initialised', function() {
+
+		it('should run the desired functionality', function() {
+
+		});
+
+	});
+
+	describe('when incorrectly initialised', function() {
+
+		it('should return the correct error', function() {
+
+		});
+
+	});
+
+});
+
+describe('test it', function() {
+
+	describe('when service exist ', function() {
+
+		it('should run the desired functionality', function() {
+
+		});
+
+	});
+
+	describe('when service not exist', function() {
+
+		it('should return the correct error', function() {
+
+		});
+
+	});
+
+});
+
